9bf81ebef4c1dd86a7f381bd130f5b232b1626cf
fifdee <fifdee@outlook.com>
2022-06-09 17:08:04 +0200

    Item model - admin register

--- okazje_app/admin.py
+++ okazje_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from okazje_app.models import Item
+
+
+class ItemAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+
+admin.site.register(Item, ItemAdmin)
