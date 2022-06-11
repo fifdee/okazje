7715c26cd471ed808e34ca45a8cdef9e68a0cf2d
fifdee <fifdee@outlook.com>
2022-06-11 13:42:05 +0200

    image_url added, image made in models save()

--- okazje_app/models.py
+++ okazje_app/models.py
@@ -1,15 +1,16 @@
-from django.core.validators import validate_comma_separated_integer_list
+from django.core.files.base import ContentFile
 from django.db import models
 from okazje_app.utils import unique_slugify
 
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField()
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField()
     price = models.CharField(max_length=100)
     url = models.URLField()
     description = models.TextField(null=True, blank=True)
-    rating = models.CharField(max_length=100, validators=[validate_comma_separated_integer_list])
+    rating = models.CharField(max_length=100)
     rating_count = models.IntegerField(null=True, blank=True)
     slug = models.SlugField(null=False, unique=True)
 
@@ -21,5 +22,12 @@ class Item(models.Model):
             slug_str = self.title[:50]
             unique_slugify(self, slug_str)
             print('unique slug: ' + str(self.slug))
+
+        if not self.image:
+            if self.image_url:
+                import requests
+                res = requests.get(self.image_url, stream=True)
+                if res.status_code == 200:
+                    self.image = ContentFile(res.raw, name='image')
 
         return super().save(*args, **kwargs)
