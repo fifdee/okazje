722507b8fdc185c60211992369d7af2774ee4f3e
fifdee <fifdee@outlook.com>
2022-06-11 13:50:55 +0200

    image_url null acceptable

--- okazje_app/models.py
+++ okazje_app/models.py
@@ -6,7 +6,7 @@ from okazje_app.utils import unique_slugify
 class Item(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(null=True, blank=True)
-    image_url = models.URLField()
+    image_url = models.URLField(null=True, blank=True)
     price = models.CharField(max_length=100)
     url = models.URLField()
     description = models.TextField(null=True, blank=True)
