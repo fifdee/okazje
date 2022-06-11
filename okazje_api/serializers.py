b94a735e1ab80bdaa6da78d9a9a99e0f02ecd107
fifdee <fifdee@outlook.com>
2022-06-11 13:16:26 +0200

    image validation (url -> download -> save raw)

--- okazje_api/serializers.py
+++ okazje_api/serializers.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from rest_framework import serializers
+from django.core.files.base import ContentFile
 
 from okazje_app.models import Item
 
@@ -16,3 +17,12 @@ class ItemSerializer(serializers.ModelSerializer):
                 raise ValidationError(message='Przedmiot o takim URL już jest w bazie danych.')
 
         return data
+
+    def validate_image(self, data):
+        import requests
+        res = requests.get(data, stream=True)
+        if res.status_code == 200:
+            image = ContentFile(res.raw, name='image')
+            return image
+        else:
+            raise ValidationError(message='Problem z pobraniem obrazka.')
