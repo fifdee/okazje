d95daf688d157506c1ea15ada68492637404a277
fifdee <fifdee@outlook.com>
2022-06-28 10:49:55 +0200

    update or create in api post

--- okazje_api/serializers.py
+++ okazje_api/serializers.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from rest_framework import serializers
+from okazje_app.models import Item
 import re
 
 from okazje_app.models import Item
@@ -10,10 +11,23 @@ class ItemSerializer(serializers.ModelSerializer):
         model = Item
         fields = ['title', 'url', 'image', 'image_url', 'price', 'rating', 'rating_count', 'description']
 
-    def validate_url(self, data):
-        items = Item.objects.all()
-        for item in items:
-            if item.url == data:
-                raise ValidationError(message='Przedmiot o takim URL już jest w bazie danych.')
+    def create(self, validated_data):
+        item, created = Item.objects.update_or_create(url=validated_data.get('url', None),
+                                                      defaults={
+                                                          'title': validated_data.get('title', None),
+                                                          'image': validated_data.get('image', None),
+                                                          'image_url': validated_data.get('image_url', None),
+                                                          'price': validated_data.get('price', None),
+                                                          'rating': validated_data.get('rating', None),
+                                                          'rating_count': validated_data.get('rating_count', None),
+                                                          'description': validated_data.get('description', None),
+                                                      })
+        return item
 
-        return data
+    # def validate_url(self, data):
+    #     items = Item.objects.all()
+    #     for item in items:
+    #         if item.url == data:
+    #             raise ValidationError(message='Przedmiot o takim URL już jest w bazie danych.')
+    #
+    #     return data
